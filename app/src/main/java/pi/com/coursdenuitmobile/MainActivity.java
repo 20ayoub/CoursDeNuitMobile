5f5352d9af7afb3041b8dce805b4a0c37c184bf0
bennouna95 <a.bennouna95@gmail.com>
2018-01-07 17:08:48 +0000

    fc

--- app/src/main/java/pi/com/coursdenuitmobile/MainActivity.java
+++ app/src/main/java/pi/com/coursdenuitmobile/MainActivity.java
@@ -7,23 +7,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import pi.com.coursdenuitmobile.R;
 import pi.com.coursdenuitmobile.entities.User;
 
 public class MainActivity extends AppCompatActivity {
     Button btnCallRestApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCallRestApi =findViewById(R.id.btnCallRestApi);
+        btnCallRestApi = findViewById(R.id.btnCallRestApi);
         btnCallRestApi.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v ){
+            public void onClick(View v) {
                 new HttpReqTask().execute();
 
 
@@ -31,32 +40,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class HttpReqTask extends AsyncTask<Void,Void,User> {
+    private class HttpReqTask extends AsyncTask<Void, Void, ArrayList<User>> {
 
         @Override
-        protected User doInBackground(Void... params) {
+        protected ArrayList<User> doInBackground(Void... params) {
 
             try {
-                String apiUrl="http://cc846dcc.ngrok.io/users/";
-                RestTemplate restTemplate= new RestTemplate();
+                URI url = new URI("http://b4ddce7b.ngrok.io/users/");
+                RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                User user= restTemplate.getForObject(apiUrl,User.class);
-                return user;
-            }catch (Exception e){
-                Log.e("",e.getMessage());
+
+                ResponseEntity<ArrayList<User>> entity = restTemplate.exchange(url, HttpMethod.GET,null,new ParameterizedTypeReference<ArrayList<User>>(){});
+                ArrayList<User> users =entity.getBody();
+
+
+                return users;
+
+            } catch (Exception e) {
+                Log.e("", e.getMessage());
 
             }
             return null;
         }
 
         @Override
-        protected void onPostExecute(User user) {
-            super.onPostExecute(user);
+        protected void onPostExecute(ArrayList<User> users) {
+            super.onPostExecute(users);
+            for(User user:users) {
 
-            Log.i("User: ","###############");
-            Log.i("user.id",String.valueOf(user.getId()));
-            Log.i("user.name",String.valueOf(user.getName()));
-
+                Log.i("User: ", "###############");
+                Log.i("user.id", String.valueOf(user.getId()));
+                Log.i("user.name", String.valueOf(user.getName()));
+            }
 
         }
     }
